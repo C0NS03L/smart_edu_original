8c3845f4c4a54334bb29480e12ebf9fad4a7b2d1
C0NS03L <c0ns03l@c0ns03l.in.th>
2025-02-12 23:21:49 +0700

    chore(deps)!: update tailwindcss (v4) and daisyui (v5)

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,25 +1,25 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+// // // // const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    "./public/*.html",
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
-    "./app/views/**/*.{erb,haml,html,slim}",
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+// // // //         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [
-    // require('@tailwindcss/forms'),
-    require("@tailwindcss/typography"),
-    // require('@tailwindcss/container-queries'),
-    require("daisyui"),
-  ],
+  // plugins: [
+  //   // require('@tailwindcss/forms'),
+  //   require("@tailwindcss/typography"),
+  //   // require('@tailwindcss/container-queries'),
+  //   require("daisyui"),
+  // ],
   daisyui: {
     themes: ["light", "dark", "coporate", "pastel"],
     darkTheme: "coporate", // name of one of the included themes for dark mode
