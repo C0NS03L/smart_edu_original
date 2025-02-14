1791320d875df4bea4eb0f1cfa010819d26fe4a2
C0NS03L <c0ns03l@c0ns03l.in.th>
2025-02-14 10:49:38 +0700

    style: remove commented out code

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,27 +1,8 @@
-// const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        // sans: ["Inter var", "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-      },
-    },
-  },
-  // plugins: [
-  //   // require('@tailwindcss/forms'),
-  //   require("@tailwindcss/typography"),
-  //   // require('@tailwindcss/container-queries'),
-  //   require("daisyui"),
-  // ],
-  // daisyui: {
-  //   themes: ["light", "dark", "coporate", "pastel"],
-  //   darkTheme: "coporate", // name of one of the included themes for dark mode
-  // },
+  ]
 };
