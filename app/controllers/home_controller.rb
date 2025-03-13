93da0284d3180ed1342077f292862637df339891
swanduckquack <minn.pai@student.mahidol.edu>
2025-03-14 00:14:18 +0700

    feat(dashboard): add staff and principal dashboard data and views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,10 @@ class HomeController < ApplicationController
     @attendance_count = scope_to_school(Attendance).count
     @last_checkin = scope_to_school(Attendance).maximum(:timestamp)
     set_student_dashboard_data if @role == 'student'
+    set_staff_dashboard_data if @role == 'staff'
+    set_principal_dashboard_data if @role == 'principal'
+    @q = Student.ransack(params[:q])
+    @students = @q.result(distinct: true)
   end
 
   private
@@ -19,5 +23,15 @@ class HomeController < ApplicationController
     @student_details = Current.user.student
     @school_details = @student_details.school
     @attendance_history = @student_details.attendances.order(created_at: :desc).limit(10)
+  end
+
+  def set_staff_dashboard_data
+    @staff_details = Current.user.staff
+    @school_details = @staff_details.school
+  end
+
+  def set_principal_dashboard_data
+    @principal_details = Current.user.principal
+    @school_details = @principal_details.school
   end
 end
