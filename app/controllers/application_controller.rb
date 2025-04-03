81a89a14d18fdbc92d6a7b0d0b528751efe3c0d8
NatthapeeSriarunluck <natthapeesri@gmail.com>
2025-04-04 01:32:22 +0700

    Revert "Merge branch 'dev' into feat/home_page"
    This reverts commit 7e697d1cb40ffd3ff8b0868c72cc2fef5609048c, reversing
    changes made to bffe9eb08dd9e65c6ecd83746d0df077abb63c65.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,7 @@
 class ApplicationController < ActionController::Base
   include Authentication
-
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
-  before_action :set_locale
-
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
-
-  def default_url_options
-    { locale: I18n.locale }
-  end
 end
 
 module SchoolScopable
